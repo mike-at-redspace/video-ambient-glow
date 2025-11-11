cf00f1ec069f1f3455b1adf55772269347c31c73
Mike Vardy <85122551+mike-at-redspace@users.noreply.github.com>
2025-11-11 08:24:00 -0500

    tweak pane reflective border

--- example/lib/tweakpaneStyles.js
+++ example/lib/tweakpaneStyles.js
@@ -13,7 +13,16 @@ export function applyTweakpaneStyles() {
       bottom: '1rem',
       right: '1rem',
       width: 'auto',
-      zIndex: '1000'
+      zIndex: '1000',
+      boxShadow: `
+        0px 0.5px 0.6px hsl(0 0% 0% / 0.06),
+        0px 1.8px 2.3px -0.5px hsl(0 0% 0% / 0.07),
+        0px 4.2px 5.3px -1.1px hsl(0 0% 0% / 0.09),
+        0px 9.7px 12.1px -1.6px hsl(0 0% 0% / 0.1)
+      `,
+      border: '2px solid light-dark(hsl(0 0% 0% / 0.1), hsl(0 0% 100% / 0.2))',
+      borderRadius: '12px',
+      backdropFilter: 'blur(8px) saturate(2.8) brightness(1.25) contrast(1.1)'
     })
   }
 }
